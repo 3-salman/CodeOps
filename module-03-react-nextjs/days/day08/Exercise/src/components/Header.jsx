406597161b328b21db64065bee1f286840620ff1
salman <7soldaz@gmail.com>
2026-09-10 10:13:47 -1200

    day08 Exercise and Mini-project

---
+++ module-03-react-nextjs/days/day08/Exercise/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useTheme } from '../context/ThemeContext'
+
+function ThemeBadge() {
+  const { theme, toggleTheme } = useTheme()
+  return (
+    <button onClick={toggleTheme} className='theme_btn'>
+      Mode: {theme}
+    </button>
+  )
+}
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1>Addis Eats</h1>
+      <ThemeBadge />
+    </div>
+  )
+}
+
+export default Header
